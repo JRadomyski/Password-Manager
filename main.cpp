a0724239741198136db5abf13908d82939686c25
jaayrad <zakubrad@gmail.com>
2023-05-14 17:12:21 +0200

    Initial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct PasswordInfo{
+    string name;
+    string category;
+    string website;
+    string login;
+};
+
+int main() {
+
+    return 0;
+}
